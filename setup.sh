bc7e1a089979f177611fb1a3d039e71772a2fa81
Doug McClure <dmcclure@biznuvo.com>
2024-10-31 23:43:07 -0400

    Add generic setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+echo "
+##
+## Choose which setup you want to run:
+##
+##   s - Basic starter setup
+##   b - Builder setup (Java, Gradle and SSH Keys)
+##   t - Basic Tomcat setup with Java
+##   z - Biznuvo setup
+##
+"
+
+read "?? Select setup type: [sbtz] " respType
+
+if [ -n "$respType" ]; then
+	case $respType in
+	s) sh -c "$(curl https://raw.githubusercontent.com/myramoki/ubuntu/main/setup-basic.sh)" ;;
+	b) sh -c "$(curl https://raw.githubusercontent.com/myramoki/ubuntu/main/setup-builder.sh)" ;;
+	t) sh -c "$(curl https://raw.githubusercontent.com/myramoki/ubuntu/main/setup-tomcat.sh)" ;;
+	z) sh -c "$(curl https://raw.githubusercontent.com/myramoki/ubuntu/main/setup-biznuvo.sh)" ;;
+	esac
+fi

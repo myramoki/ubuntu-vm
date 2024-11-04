97cf4b532a5124ffe7f3e85639fbe812cce4515d
Doug McClure <dmcclure@biznuvo.com>
2024-11-04 16:53:31 -0500

    Fix config for ssh to github

--- 602-github-ssh-cfg.sh
+++ 602-github-ssh-cfg.sh
@@ -1,15 +1,18 @@
 printf "\n\n#### Updating BN user information\n\n\n"
 
 echo "[core]
-        autocrlf = false
+    autocrlf = false
 " > ~bn/.gitconfig
 chown bn: ~bn/.gitconfig
 
-curl -sL https://raw.githubusercontent.com/myramoki/ubuntu-vm/main/biznuvo-repos-sshkeys.txz.gpg | gpg -d | tar -J -tvf - -C ~/.ssh/
+curl -sL -o /tmp/ssh-keys https://raw.githubusercontent.com/myramoki/ubuntu-vm/main/biznuvo-repos-sshkeys.txz.gpg
+gpg -d /tmp/ssh-keys | tar -J -tvf - -C ~/.ssh/
 
-echo "Host server-v2
-        Hostname github.com
-        IdentityFile=~/.ssh/biznuvo-server-v2-id_ed255519
+echo "
+Host github.com-server-v2
+    Hostname ssh.github.com
+    Port 443
+    IdentityFile=~/.ssh/biznuvo-server-v2-id_ed255519
 " > ~bn/.ssh/config
 
 chown bn: ~bn/.ssh/*
